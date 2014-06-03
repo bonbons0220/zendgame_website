c2ef0e943fe242e9ed1ec59f2c2c56bec964c502
Bonnie Souter <bonbons0220@gmail.com>
2014-06-02 20:43:45 -0400

    Changed images on home page. Started changing the copy. Need to write the additional pages before writing more content.

--- pages/homeinc.php
+++ pages/homeinc.php
@@ -1,11 +1,11 @@
 		<div class="wrapper col3">
 		  <div id="intro">
 			<div class="fl_left">
-				<h2>yourwebsite.com</h2>
-				<p>is just the beginning. We create, update, and overhaul Websites, Blogs, Content Management, add Dynamic Content, build Customer Relationship Management tools, create Social Media interfaces, and more.<br />&nbsp;</p>
+				<h2>mywebsite.com</h2>
+				<p>...is just the beginning. We create, update, and overhaul Websites, Blogs, Content Management, add Dynamic Content, build Customer Relationship Management tools, create Social Media interfaces, and more.<br />&nbsp;</p>
 				<!--p class="readmore"><a href="#">Read more about what we offer. </a></p-->
 			</div>
-			<div class="fl_right"><img src="/images/html.jpg" alt="" /></div>
+			<div class="fl_right"><img src="/images/website.jpg" alt="" /></div>
 			<br class="clear" />
 		  </div>
 		</div>
@@ -13,30 +13,30 @@
 		  <div id="services">
 			<ul>
 			  <li>
-				<div class="fl_left"><img src="images/website.jpg" alt="" /></div>
+				<div class="fl_left"><img src="images/html.jpg" alt="" /></div>
 				<div class="fl_right">
-				  <h2>Show Off</h2>
-				  <p>It all starts here. Your website is the most fundamental building block of your online presence: your shopfront window; 
-				  your first impression. Let us make it exceptional.</p>
+				  <h2>Getting Set Up</h2>
+				  <p>Where do you start? What kind of website you need? There are many 
+				  options to consider in setting up your website.</p>
 				  <!--p class="readmore"><a href="#">Continue Reading &raquo;</a></p-->
 				</div>
 			  </li>
 			  <li>
 				<div class="fl_left"><img src="images/blog.jpg" alt="" /></div>
 				<div class="fl_right">
-				  <h2>Blog</h2>
-				  <p>Providing relevant online content underscores your expertise. We can help you set up Content Management Software
-				  (such as WordPress), putting your content in your hands.</p>
+				  <h2>Managing Content</h2> 
+				  <p>Providing relevant content underscores your expertise to your customers. There are
+				  many tools that make creating and publishing your content on your website simple
+				  and painless.</p>
 				  <!--p class="readmore"><a href="#">Continue Reading &raquo;</a></p-->
 				</div>
 			  </li>
 			  <li class="last">
 				<div class="fl_left"><img src="images/social.jpg" alt="" /></div>
 				<div class="fl_right">
-				  <h2>Connect</h2>
-				  <p>There are so many ways to connect with your customers the mind boggles. 
-					We can help you automate the process of pushing your content to social media such as
-					Twitter, Twilio, Tumblr, Facebook.</p>
+				  <h2>Connecting</h2>
+				  <p>There are many ways to connect with your customers through social media, email
+				  lists, newsletters, Phones and more. Which are most effective and how do you set them up?</p>
 				  <!--p class="readmore"><a href="#">Continue Reading &raquo;</a></p-->
 				</div>
 			  </li>
